0642d454cb2dd2391be5ccc995c722278017ddb3
pitbrest <sasha.petrov@tut.by>
2022-10-11 16:11:03 +0300

    feat: implement update planet in 4sec and loading img whyle planet is updating

--- src/components/random-planet/Random-planet.jsx
+++ src/components/random-planet/Random-planet.jsx
@@ -1,100 +1,83 @@
 import React, { Component } from 'react';
 import './Random-planet.css';
 import { Loading } from '../loading/Loading';
-
 import { ApiResources } from '../api-resurses/Api-Resurs';
 
 class RandomPlanet extends Component {
+	state = {
+		planet: {},
+		isLoading: true,
+	};
 
-  state = {
-    planet: {},
-    isLoading: true
-  };
+	myApi = new ApiResources();
 
-  myApi = new ApiResources();
+	getRandomIntInclusive = (min, max) => {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
 
-  getRandomIntInclusive = (min = 2, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+	getId = async () => {
+		const dataLength = await this.myApi.getPlanets().then((data) => data.length);
+		return this.getRandomIntInclusive(2, dataLength);
+	};
 
-  getId = async () => {
-    await this.myApi.getPlanets().then((data) => {
-      this.setState({
-        id: this.getRandomIntInclusive(2, data.length),
-      });
-    });
-  };
+	updatePlanet = async () => {
+		await this.myApi.getPlanet(await this.getId()).then((planet) =>
+			this.setState({
+				planet: { ...planet, img: `https://starwars-visualguide.com/assets/img/planets/${planet.id}.jpg` },
+				isLoading: false,
+			})
+		);
+	};
 
-  updatePlanet = async () => {
-    await this.getId();
-    this.myApi.getPlanet(this.state.id).then((planet) => {
-      this.setState({
-        isLoading: false,
-        planet: {
-          ...planet,
-          img: `https://starwars-visualguide.com/assets/img/planets/${this.state.id}.jpg`,
-        }
-      });
-    });
-  };
+	planetSlider = () => {
+		setInterval(() => {
+			this.updatePlanet();
+		}, 4000);
+	};
 
-  async componentDidMount() {
-    await this.getId();
-    this.updatePlanet();
-    this.myFunc();
-  }
+	componentDidMount() {
+		this.updatePlanet();
+		this.planetSlider();
+	}
 
-  myFunc() {
-    setInterval(async () => {
-      await this.getId();
-      this.updatePlanet();
-    }, 4000);
-  }
-
-  render() {
-    const { planet, isLoading } = this.state;
-
-
-    return (
-      <div className='randomPlanet-container'>
-        <Content planet={planet} isLoading={isLoading} />
-      </div>
-    );
-  }
+	render() {
+		const { planet, isLoading } = this.state;
+		return (
+			<div className='randomPlanet-container'>
+				<SectionView planet={planet} isLoading={isLoading} />
+			</div>
+		);
+	}
 }
 
 export { RandomPlanet };
 
-
-const Content = ({ planet, isLoading }) => {
-  const { img, planetName, population, rotationPeriod, diameter } = planet;
-
-  return (
-    <>
-      {
-        isLoading ? <Loading /> :
-          <>
-            <img src={img} className='planet-img' alt='planet-img' />
-            <div className='planetInfo-container'>
-              <h2 className='planet-title'>{planetName}</h2>
-              <div className='planet-info'>
-                <p className='planetInfo-text'>
-                  Population <span>{population}</span>
-                </p>
-                <p className='planetInfo-text'>
-                  Rotation Period <span>{rotationPeriod}</span>{' '}
-                </p>
-                <p className='planetInfo-text'>
-                  Diameter <span>{diameter}</span>
-                </p>
-              </div>
-            </div>
-          </>
-      }
-    </>
-  );
+const SectionView = ({ planet, isLoading }) => {
+	return (
+		<>
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					<img src={planet.img} className='planet-img' alt='planet-img' />
+					<div className='planetInfo-container'>
+						<h2 className='planet-title'>{planet.planetName}</h2>
+						<div className='planet-info'>
+							<p className='planetInfo-text'>
+								Population <span>{planet.population}</span>
+							</p>
+							<p className='planetInfo-text'>
+								Rotation Period <span>{planet.rotationPeriod}</span>
+							</p>
+							<p className='planetInfo-text'>
+								Diameter <span>{planet.diameter}</span>
+							</p>
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
 };
-
-
